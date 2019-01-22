0a0a103da476171583d04944e8eb47e76b1ea751
zhoujundi <shouji18072834696@outlook.com>
2019-01-22 22:14:33 +0800

    docker

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,15 +11,15 @@ import java.time.LocalDateTime;
 public class DemoApplication {
     public static LocalDateTime firstTime ;
     private static org.slf4j.Logger Logger = LoggerFactory.getLogger(DemoApplication.class);
-    static {
-        String path1 = System.getProperty("user.dir");
-        PropertyConfigurator.configure(path1 + "/src/main/resources/log4j.properties");
-    }
+
 
     public static void main(String[] args) {
+        int count = 0;
         SpringApplication.run(DemoApplication.class, args);
-        Logger.info("asdfasdf{}","程序启动成功！");
+        Logger.warn("asdfasdf{}","程序启动成功！");
+
         firstTime = LocalDateTime.now();
+
     }
 
 }
--- src/main/java/com/example/demo/controller/TimeController.java
+++ src/main/java/com/example/demo/controller/TimeController.java
@@ -1,13 +1,19 @@
 package com.example.demo.controller;
 
+import com.example.demo.DemoApplication;
+
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TimeController {
-
+    static int count = 0;
+    private static org.slf4j.Logger Logger = LoggerFactory.getLogger(TimeController.class);
     @GetMapping("/time")
     public String time(){
+        count++;
+        Logger.warn("第"+count+"次调time");
         return "after 2 minutes!";
     }
 }
